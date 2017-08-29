507cfc5fc357ae4b0bee0d8a3084c0dbefa983aa
seyi-adeleke <adetokunbo.adeleke@andela.com>
2017-08-29 12:38:24 +0100

    refactor(index)
    Refactored the index file

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
 #! /usr/bin/env node
 
 'use strict';
-const program  = require("commander");
+const program = require("commander");
 
 //http client library
-const request  = require("request");
+const request = require("request");
+const currentVersion = '1.0.0';
 
 // return the help menu if no argument was inputed
-if(process.argv.length === 2){
+if (process.argv.length === 2) {
     program.help();
 }
-
-/*
- this function uses the request package to get info from the url
- process.argv[2] is the input from the command line
-
-*/
-
-let getState = request('https://nigerian-states-info.herokuapp.com/api/states/'+ process.argv[2].toLowerCase() , function (error, response, data) {
-    if(error){
+if (process.argv.length > 3) {
+    console.log('You can only search for one state.')
+    return;
+}
+const apiUrl = 'https://nigerian-states-info.herokuapp.com/api/states/'
+let getState = request(apiUrl + process.argv[2].toLowerCase(), (error, response, data) => {
+    if (error) {
         console.log("There is an error");
         console.log(error);
-
     }
     else {
-    console.log("searching...");
-    // parse the JSON data received
-    let json = JSON.parse(data);
-    //log a success message
-    console.log(json.info);
-
-    if(json.info != "state not found"){
-        console.log("State Name: " + json.data.info.officialName);
-        console.log("Capital: " + json.data.info.Capital);
-        console.log("Governor: " + json.data.info.Governor);
-        console.log("Deputy Governor: " + json.data.info.DeputyGovernor);
-        console.log("Population: " + json.data.info.Population + " people");
-        console.log("Slogan: " + json.data.info.Slogan);
-        console.log("Number of Local Government Areas: " + json.data.info.Number_of_LGAS);
-        console.log("Official Website: " + json.data.info.Website);
-    }
-    // if state is not found log a message to the user
-    else{
-        console.log("You inputed a wrong state name, please try again");
-    }
+        let json = JSON.parse(data);
+        console.log(json.info);
+        if (json.info !== "state not found") {
+            console.log("State Name: " + json.data.info.officialName);
+            console.log("Capital: " + json.data.info.Capital);
+            console.log("Governor: " + json.data.info.Governor);
+            console.log("Deputy Governor: " + json.data.info.DeputyGovernor);
+            console.log("Population: " + json.data.info.Population + " people");
+            console.log("Slogan: " + json.data.info.Slogan);
+            console.log("Number of Local Government Areas: " + json.data.info.Number_of_LGAS);
+            console.log("Official Website: " + json.data.info.Website);
+        }
+        else {
+            console.log("You inputed a wrong state name, please try again");
+        }
     }
 });
 
 
 program
-    .version('1.0.0')
+    .version(currentVersion)
     .command('stateinfo [state]')
     .description('get state information')
     .action(getState);
+
+program
+    .version(currentVersion)
+    .option('-v, --version', 'get version')
+
+
 
 program.parse(process.argv);
